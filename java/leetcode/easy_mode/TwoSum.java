cc3dc431392ee70b63b01ab628ddb0564a5cb635
ICEcoldShallom <somtoezeoke.develop@gmail.com>
2025-06-15 17:02:46 +0100

    Attempt Two - TwoSum.java

--- java/leetcode/easy_mode/TwoSum.java
+++ java/leetcode/easy_mode/TwoSum.java
@@ -1,11 +1,10 @@
-/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
 You can return the answer in any order.
 
 Example 1:
-
 Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]
@@ -19,6 +18,8 @@ Input: nums = [3,3], target = 6
 Output: [0,1] */
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -36,10 +37,26 @@ public class TwoSum {
         System.out.println("Solution: " + Arrays.toString(output));
     }
 
+
+    static void attemptTwoTwoSum(int[] listofNums, int target){
+        Map<Integer, Integer> graph = new HashMap<>();
+
+        for(int i = 0; i < listofNums.length; i++){
+            int result = target - listofNums[i];
+            if(graph.containsKey(result)){
+                System.out.println("Solution: " + Arrays.toString(new int[]{graph.get(result), i}));
+            } else {
+                graph.put(listofNums[i], i);
+            }
+        }
+    }
+
+
     public static void main(String[] args){
          int[] nums = {2,7,11,15};
          int target = 9;
 
-         attemptOneTwoSum(nums,target); // 49 milliseconds with a complexity of O(n^2)
+         //attemptOneTwoSum(nums,target); // 49 milliseconds with a complexity of O(n^2)
+           attemptTwoTwoSum(nums, target); // 3 milliseconds with a complexity of O(n)
     }
 }
